4cb0f96c93cf4dbd853288389081d8d51860f7c8
miao <miao@163.com>
2021-07-31 04:21:08 +0800

    简化结构

--- src/main.js
+++ src/main.js
@@ -1,13 +1,4 @@
 import "./audioControlEventsInit"
 import "./style.css"
-import '../assets/images/next.png'
-import '../assets/images/play.png'
-import '../assets/images/previous.png'
-import '../assets/images/random_play.png'
-import '../assets/images/sequence_play.png'
-import '../assets/images/simple_cycle_play.png'
-import '../assets/images/stop.png'
-import '../assets/images/volume.png'
-import '../assets/images/music_list.png'
 import "./musicDataInit"
 import "./frequencyInit"
